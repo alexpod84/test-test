c3a4e61923f9d0615dd98d7b12b7611ece259d41
alexpod84 <64498964+alexpod84@users.noreply.github.com>
2022-07-22 16:26:03 +0300

    BasePage.java

---
+++ src/test/java/tests/ibtest/SignUp.java
@@ -0,0 +1,43 @@
+package tests.ibtest;
+
+
+//import org.junit.Test;
+import org.testng.annotations.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import pages.ibpage.IbPage;
+import tests.base.BaseTest;
+
+
+import java.time.Duration;
+
+import static constants.Constant.Urls.TEST_URL;
+import static constants.Constant.Urls.PROD_URL;
+import static constants.Constant.logpass.LOGIN_TEST;
+import static constants.Constant.logpass.PASSWORLD_TEST;
+public class SignUp extends BaseTest {
+
+
+    @Test(priority = 1, description = "Вход")
+
+    public void authorization() {
+
+        basePage.open(TEST_URL);
+
+
+        ibPage
+                .authorization(LOGIN_TEST, PASSWORLD_TEST);
+
+    }
+    @Test(priority = 2, description = "Открытие платежного поручения")
+
+    public void openDocBook() {
+        docBook
+                .openDocBook()
+                .openDocList()
+                .openBayDocBy();
+    }
+}
