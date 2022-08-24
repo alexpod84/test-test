fb1136d14831fe3e15b584ad54a42a52b12f380d
alexpod84 <64498964+alexpod84@users.noreply.github.com>
2022-08-24 15:47:35 +0300

    заполнение полей пп

--- src/main/java/pages/ibpage/PayDocBy.java
+++ src/main/java/pages/ibpage/PayDocBy.java
@@ -1,9 +1,27 @@
 package pages.ibpage;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import pages.base.BasePage;
 
 public class PayDocBy extends BasePage {
+
+        private final By inamount = By.xpath("//div[@class='input-amount']/input");
+        private final By payerTypecb = By.xpath("//h2[text()='Плательщик']//following-sibling::div//i[@class='sc-icon sc-icon-arrow-down ng-star-inserted']");
+
+
+        private final By payerType = By.xpath("//div[text()='INI - Индивидуальный предприниматель']");
+
         public PayDocBy (WebDriver driver) {super(driver); }
 
+        public PayDocBy inputData () {
+                WebElement weinamount = driver.findElement(inamount);
+                waitElementIsVisible(weinamount).sendKeys("1");
+                WebElement wepayerTypecb = driver.findElement(payerTypecb);
+                waitElementIsVisible(wepayerTypecb).click();
+                WebElement wepayerType = driver.findElement(payerType);
+                waitElementIsVisible(wepayerType).click();
+                return this;
+        }
 }
